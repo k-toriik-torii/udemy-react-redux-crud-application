fd06694ab7ff292f6e31350699c35171de95e16a
k-toriik-torii <k-torii@rhizome-e.com>
2021-07-31 22:01:55 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import {Component} from "react";
-import logo from './logo.svg';
+import React, {Component} from "react";
 
 
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>
+    return (
+        <React.Fragment>
+          <label htmlFor="bar">bar</label>
+          <input type="text" onChange={() => {console.log("I am clicked.")}} />
+        </React.Fragment>
+    )
+
   }
 }
 
